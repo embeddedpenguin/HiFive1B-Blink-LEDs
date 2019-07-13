4139c08897f2f37036b695576833847a65fbc313
Penguin <ad0097@uah.edu>
2019-07-13 04:35:03 -0500

    led blink works, but the debugger in platformio does not. which is weird

--- include/project.h
+++ include/project.h
@@ -7,12 +7,14 @@
 #define OFF             0x00
 #define NUM_LEDS        0x03
 
-#define RED_LED         0x400000
-#define BLUE_LED        0x200000
-#define GREEN_LED       0x080000
+#define RED_LED          0x400000
+#define BLUE_LED         0x200000
+#define GREEN_LED        0x080000
+
 
 void setup_gpio(void);
 int set_led(int color, int state);
 void delay(int ms);
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
-
 #include "project.h"
-void setup_gpio(void)
-{
-    return;
-}
-int set_led(int color, int state)
-{
-    return 0;
-}
-void delay(int ms)
-{
-    return;
-}
+
 int main()
 {
-    int error = 0;
+    volatile int error = 0;
     int led_num = 0;
     int colors[NUM_LEDS] = {GREEN_LED, RED_LED, BLUE_LED};
-
+    printf("Got here\n");
     setup_gpio();
 
     for(;;)
     {
+        
         while(!error)
         {
+            printf("Hello\n");
             set_led(colors[led_num], ON);
             delay(EXAMPLE_DELAY);
             error = set_led(colors[led_num], OFF);
